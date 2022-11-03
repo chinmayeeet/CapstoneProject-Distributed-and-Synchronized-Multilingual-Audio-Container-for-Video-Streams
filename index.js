fa256c8f430a62f7130d59b0e68dcaee775adcb9
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-11-03 18:24:51 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
+
+app.get('/', function (req, res) {
+    res.send('GET request to homepage')
+})
+
+  app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+}); 
+
+
